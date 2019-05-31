b67aa8d7368929ade52d6f39cb6cce4e0549fdd5
Quang Vu <vuquangit@gmail.com>
2019-05-31 11:55:20 +0700

    Add item Basic Javascript

---
+++ src/Javascript Algorithms And Data Structures Certification (300 hours)/Basic JavaScript/1.63 Delete Properties from a JavaScript Object.js
@@ -0,0 +1,43 @@
+/* https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-javascript/delete-properties-from-a-javascript-object
+
+We can also delete properties from objects like this:
+    delete ourDog.bark;
+
+Delete the "tails" property from myDog. You may use either dot or bracket notation.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+...
+*/
+
+// Setup
+var myDog = {
+  name: "Happy Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+  bark: "woof"
+};
+
+// Only change code below this line.
+
+delete myDog["tails"];
