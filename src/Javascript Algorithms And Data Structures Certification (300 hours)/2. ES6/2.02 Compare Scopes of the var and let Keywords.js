fa6ccd738597b89cc7bac828b298c84febe9d44e
Quang Vu <vuquangit@gmail.com>
2019-06-01 16:47:17 +0700

    Add item of ES6

--- src/Javascript Algorithms And Data Structures Certification (300 hours)/2. ES6/2.02 Compare Scopes of the var and let Keywords.js
+++ src/Javascript Algorithms And Data Structures Certification (300 hours)/2. ES6/2.02 Compare Scopes of the var and let Keywords.js
@@ -60,22 +60,29 @@ was created in the for loop. The let keyword does not follow this behavior:
     console.log(i);
     // returns "i is not defined"
 
-i is not defined because it was not declared in the global scope. It is only declared within the for loop statement. printNumTwo() returned the correct value because three different i variables with unique values (0, 1, and 2) were created by the let keyword within the loop statement.
+i is not defined because it was not declared in the global scope. 
+It is only declared within the for loop statement. printNumTwo() returned the correct value 
+because three different i variables with unique values (0, 1, and 2) were created by the let 
+keyword within the loop statement.
 
 
-Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of the function. Be certain not to use the var keyword anywhere in your code.
+Fix the code so that i declared in the if statement is a separate variable than i declared in 
+the first line of the function. Be certain not to use the var keyword anywhere in your code.
 
-This exercise is designed to illustrate the difference between how var and let keywords assign scope to the declared variable. When programming a function similar to the one used in this exercise, it is often better to use different variable names to avoid confusion.
-function checkScope() {
-"use strict";
-  var i = "function scope";
-  if (true) {
-    i = "block scope";
-    console.log("Block scope i is: ", i);
-  }
-  console.log("Function scope i is: ", i);
-  return i;
-}
+This exercise is designed to illustrate the difference between how var and let keywords assign 
+scope to the declared variable. When programming a function similar to the one used in this exercise, 
+it is often better to use different variable names to avoid confusion.
+
+    function checkScope() {
+    "use strict";
+    var i = "function scope";
+    if (true) {
+        i = "block scope";
+        console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+    }
 */
 
 function checkScope() {
