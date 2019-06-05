517fac3c2b4bf749dbf143b37c23a5a7f560794e
Quang Vu <vuquangit@gmail.com>
2019-06-05 16:59:36 +0700

    Add item of ES6 and Regular Expression

---
+++ src/Javascript Algorithms And Data Structures Certification (300 hours)/3. Regular Expression/3.00 Introduction to the Regular Expression Challenges.js
@@ -0,0 +1,13 @@
+/*
+Regular expressions are special strings that represent a search pattern. Also known as "regex" 
+or "regexp", they help programmers match, search, and replace text. Regular expressions can 
+appear cryptic because a few characters have special meaning. The goal is to combine the symbols 
+and text into a pattern that matches what you want, but only what you want. This section will 
+cover the characters, a few shortcuts, and the common uses for writing regular expressions.
+*/
+
+/*
+Reference:
+https://developer.mozilla.org/vi/docs/Web/JavaScript/Guide/Regular_Expressions
+https://viblo.asia/p/bieu-thuc-chinh-quy-regex-trong-javascript-QpmleQ9mlrd
+*/
