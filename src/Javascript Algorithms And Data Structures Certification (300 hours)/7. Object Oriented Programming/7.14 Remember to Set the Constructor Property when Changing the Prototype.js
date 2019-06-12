733576178bcc1dec97699f947ec701b66cc13a6a
Quang Vu <vuquangit@gmail.com>
2019-06-12 16:47:39 +0700

    Add item of Object Oriented Programming

---
+++ src/Javascript Algorithms And Data Structures Certification (300 hours)/7. Object Oriented Programming/7.14 Remember to Set the Constructor Property when Changing the Prototype.js
@@ -0,0 +1,45 @@
+/*
+https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/object-oriented-programming/remember-to-set-the-constructor-property-when-changing-the-prototype
+
+Object Oriented Programming: Remember to Set the Constructor Property when Changing the Prototype
+
+There is one crucial side effect of manually setting the prototype to a new object. It erased 
+the constructor property! The code in the previous challenge would print the following for duck:
+
+    console.log(duck.constructor)
+    // prints ‘undefined’ - Oops!
+    
+To fix this, whenever a prototype is manually set to a new object, remember to define the 
+constructor property:
+
+    Bird.prototype = {
+        constructor: Bird, // define the constructor property
+        numLegs: 2,
+        eat: function() {
+            console.log("nom nom nom");
+        },
+        describe: function() {
+            console.log("My name is " + this.name); 
+        }
+    };
+
+---------------------------------------------------------------------------------------
+Define the constructor property on the Dog prototype.
+
+    function Dog(name) {
+        this.name = name; 
+    }
+
+    // Modify the code below this line
+    Dog.prototype = {
+    
+        numLegs: 2, 
+        eat: function() {
+            console.log("nom nom nom"); 
+        }, 
+        describe: function() {
+            console.log("My name is " + this.name); 
+        }
+    };
+
+*/
