439519e954d947f225f24c3011888f305302bdbe
vuquangit <vuquangit@gmail.com>
2019-06-21 22:42:23 +0700

    Add JavaScript Algorithms&Data Structures Projects

---
+++ src/Javascript Algorithms And Data Structures Certification (300 hours)/10. JavaScript Algorithms and Data Structures Projects/10.02 Roman Numeral Converter.js
@@ -0,0 +1,15 @@
+/*
+https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter
+
+JavaScript Algorithms and Data Structures Projects: Roman Numeral Converter
+
+Convert the given number into a roman numeral.
+All roman numerals answers should be provided in upper-case.
+
+    function convertToRoman(num) {
+        return num;
+    }
+
+    convertToRoman(36);
+
+*/
