e84f3b0a9a5956c1a71216eb57467b64b044ab6a
Quang Vu <vuquangit@gmail.com>
2019-06-27 17:03:49 +0700

    Add jQuery

---
+++ src/Front End Libraries Certification (300 hours)/2. jQuery/2.00 Introduction to jQuery.js
@@ -0,0 +1,9 @@
+/*
+https://learn.freecodecamp.org/front-end-libraries/jquery
+
+Introduction to jQuery
+
+jQuery is one of the many libraries for JavaScript. It is designed to simplify scripting done 
+on the client side. jQuery's most recognizable characteristic is its dollar sign ($) syntax. 
+With it, you can easily manipulate elements, create animations and handle input events.
+*/
